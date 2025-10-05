3a006c7ec823f5af69e1acc7a404e482bdb16e47
rupjyoti921 <rupjyoti921@gmail.com>
2025-10-05 23:31:06 +0530

    learned about ProgressCallBack

---
+++ LearnOOPinC#/CSharpPractice/CSharpPractice/18ProgressCallBack/1PCB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CshOOPPractice._18ProgressCallBack
+{
+    public class PCB
+    {
+        public static async Task Main2()
+        {
+            var progress = new Progress<int>(percent =>
+            {
+                Console.Write($"\rProgress is : {percent}%");
+            });
+
+            await StartTheTest( progress );
+        }
+
+        public static async Task StartTheTest(IProgress<int> progress)
+        {
+            Console.WriteLine("Test is Running..");
+            for (int i = 0; i <= 100; )
+            {
+                progress?.Report(i);
+                await Task.Delay(500);
+                i += 10;
+
+            }
+            Console.WriteLine("\n------Test Completed-------");
+        }
+
+
+    }
+}
